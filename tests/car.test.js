a9b4d963b24839a67d9912778b698338a364ca2a
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-04 14:29:38 +0100

    attempted TDD on car test

---
+++ tests/car.test.js
@@ -0,0 +1,14 @@
+test ('has name', () => {
+    // Arrange - do any initial setup
+    const expected = "Mercedes Benz"
+    // Assert - check it worked the way we expect
+    expect("Mercedes Benz").toBe(expected);
+
+});
+
+test('can set name',() => {
+
+   // Act - do the thing we want to test
+   const actual = ("Mclaren")
+
+});
