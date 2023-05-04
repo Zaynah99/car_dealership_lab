6afeca761e7a17a02351da393c145f0ec5bff666
Zaynah <zaynah.sadiq@hotmail.com>
2023-05-04 14:58:09 +0100

    created dealership class and test

---
+++ dealership.js
@@ -0,0 +1,5 @@
+const Dealership = function (name, maxNoOfCars, carsInStock){
+    this.name = name;
+    this.maxNoOfCars = maxNoOfCars;
+    this.carsInStock = carsInStock;
+}
--- tests/car.test.js
+++ tests/car.test.js
@@ -3,14 +3,13 @@ const Car = require('./car');
 test ('has name', () => {
     // Arrange - do any initial setup
     const expected = audi
-    //    // Act - do the thing we want to test
+    // Act - do the thing we want to test
     const actual = 
     // Assert - check it worked the way we expect
     expect(audi.manufacturer).toBe(expected);
 
 });
 
-// test('can set name',() => 
 
 
 
